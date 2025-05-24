d9c8b834f92625e717f77331cf60fa075409ccec
hyy <ssuzi97@gmail.com>
2025-05-24 14:15:19 +0900

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -1,106 +1,31 @@
-# IoT Network Lab - AWS VPC Terraform Template
+# root main.tf
 
 provider "aws" {
-  region = "ap-northeast-2"  # 서울 리전
+  region = var.aws_region
 }
 
-# VPC 생성
-resource "aws_vpc" "iot_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support  = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "iot-vpc"
-  }
+module "vpc" {
+  source            = "./modules/vpc"
+  project           = var.project
+  vpc_cidr          = var.vpc_cidr
+  cctv_subnet_cidr  = var.cctv_subnet_cidr
+  temp_subnet_cidr  = var.temp_subnet_cidr
 }
 
-# Subnet - CCTV (VLAN 10 대응)
-resource "aws_subnet" "cctv_subnet" {
-  vpc_id                  = aws_vpc.iot_vpc.id
-  cidr_block              = "10.0.10.0/24"
-  availability_zone       = "ap-northeast-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "cctv-subnet"
-  }
+module "iam" {
+  source = "./modules/iam"
 }
 
-# Subnet - TempSensor (VLAN 20 대응)
-resource "aws_subnet" "tempsensor_subnet" {
-  vpc_id                  = aws_vpc.iot_vpc.id
-  cidr_block              = "10.0.20.0/24"
-  availability_zone       = "ap-northeast-2a"
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "tempsensor-subnet"
-  }
+module "ec2" {
+  source              = "./modules/ec2"
+  cctv_subnet_id      = module.vpc.cctv_subnet_id
+  temp_subnet_id      = module.vpc.temp_subnet_id
+  iam_instance_profile = module.iam.ec2_profile_name
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.iot_vpc.id
-  tags = {
-    Name = "iot-igw"
-  }
-}
-
-# Route Table - CCTV용 (인터넷 접근 가능)
-resource "aws_route_table" "cctv_rt" {
-  vpc_id = aws_vpc.iot_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "cctv-rt"
-  }
-}
-
-resource "aws_route_table_association" "cctv_rta" {
-  subnet_id      = aws_subnet.cctv_subnet.id
-  route_table_id = aws_route_table.cctv_rt.id
-}
-
-# 보안 그룹 - CCTV
-resource "aws_security_group" "cctv_sg" {
-  name        = "cctv-sg"
-  description = "Allow RTSP"
-  vpc_id      = aws_vpc.iot_vpc.id
-
-  ingress {
-    description = "Allow RTSP"
-    from_port   = 554
-    to_port     = 554
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# 보안 그룹 - TempSensor
-resource "aws_security_group" "tempsensor_sg" {
-  name        = "tempsensor-sg"
-  description = "Allow MQTT"
-  vpc_id      = aws_vpc.iot_vpc.id
-
-  ingress {
-    description = "Allow MQTT"
-    from_port   = 1883
-    to_port     = 1883
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.10.0/24"] # CCTV subnet에서만 허용
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "nat" {
+  source            = "./modules/nat"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_id  = module.vpc.cctv_subnet_id
+  private_subnet_id = module.vpc.temp_subnet_id
 }
